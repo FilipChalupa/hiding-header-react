7c8ac267316594ac10a0d42f91cff907737651b7
Filip Chalupa <chalupa.filip@gmail.com>
2023-01-20 14:47:11 +0100

    Extract code to separate file

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import typescript from 'rollup-plugin-typescript2'
 import packageJson from './package.json'
 
 export default {
-	input: './src/index.tsx',
+	input: './src/index.ts',
 	output: [
 		{
 			file: packageJson.main,
